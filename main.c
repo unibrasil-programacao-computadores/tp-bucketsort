83f011cabfb031951591d1eed2f7a1f3327e3a58
Natã Bonfante de Oliveira <darkin.aura@gmail.com>
2025-06-02 19:12:14 -0300

    Add files via upload

--- main.c
+++ main.c
@@ -1,38 +1,32 @@
 #include <stdio.h>
-#include <bucketsort.h>
+#include <stdlib.h>
+#include <time.h>
+#include <math.h>
+#include "bucketsort.h"
 
-int main(){
+int main() {
+       int vetor[TAM_VETOR];
 
-    void bucketSort(int vet[], int tam){
-        Balde b[numBaldes];
-        int i, j, k;
-        for(i=0;i<=numBaldes;i++) {
-            b[i].topo = 0;
-        }
-        //verifica balde
-        for(i=0;i<tam;i++) {
-            j=numBaldes;
-            while(j>0){
-                if(vet[i] < j*10) {
-                b.[j].balde[b[j].topo] = vet[i];
-                b[j].topo=b[j].topo + 1;
-                break;
-            }
-        j--;
-        }
-        }
-        for(i=0; i<=numBaldes;i++){
-            if(b[i].topo>0){
-                void bubbleSort(b[i].balde, b[i].topo); //Método de ordenação (implementar)
-            }
-        }
-        //Retorna os valores ao vetor original
-        for(j=0;j<numBaldes;j++){
-            for(k=0; k<b[j].topo;k++){
-                vet[i]=b[j].balde[k];
-                i++
-            }
-        }
+srand((unsigned int)time(NULL));
+
+ for (int i = 0; i < TAM_VETOR; ++i)
+    {
+        vetor[i] = rand() % 100;
     }
-     void bubbleSort(int vet[], int tam);
+
+printf("vetor desordenado:");
+    for (int i = 0; i < TAM_VETOR; ++i)
+     {
+          printf("%d%c", vetor[i], (i < TAM_VETOR - 1) ? ' ' : '\n');
+}
+
+  ordena_baldes(vetor, TAM_VETOR);
+
+   printf("\n Vetor ordedado:");
+      for (int i = 0; i < TAM_VETOR; ++i)
+       {
+    printf("%d%c", vetor[i], (i < TAM_VETOR - 1) ? ' ' : '\n');
+    }
+
+    return 0;
 }
